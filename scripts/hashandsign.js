c0719300085bbe387e1438fb09664cbe5cce34bf
yanrusu <yanrusu0701@gmail.com>
2025-12-15 17:35:57 +0800

    update hashandsign.js different way to sign message

--- scripts/hashandsign.js
+++ scripts/hashandsign.js
@@ -3,12 +3,15 @@ const { ethers } = require("hardhat");
 async function main() {
     const msg = "Hello world";
     const msg_bytes = ethers.toUtf8Bytes(msg);
-    const hash_msg = ethers.keccak256(msg_bytes);
     const wallet = ethers.Wallet.createRandom();
     const walletadderss = wallet.address;
     
-    const sign = await wallet.signMessage(hash_msg);
-    const addr = await ethers.verifyMessage(hash_msg, sign);
+    // const sign = await wallet.signMessage(msg_bytes); // 內建hash加EIP-191前綴辨識簽名
+    // const addr = await ethers.verifyMessage(msg_bytes, sign);
+
+    const hash_msg = ethers.keccak256(msg_bytes);
+    const signature = wallet.signingKey.sign(hash_msg);  // 直接簽 hash 回傳物件
+    const addr = ethers.recoverAddress(hash_msg, signature.serialized); //signature.serialized組合簽名
     
     console.log("recovered:", addr);
     console.log("wallet:", walletadderss);
